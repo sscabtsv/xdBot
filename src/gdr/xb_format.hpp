239651903ef1f31423d2ef3b31c5d70d4bab1817
sscabtsv <anastacioaijhenweina@gmail.com>
2026-08-18 06:12:42 +0800

    Add missing newline at end of xb_format.hpp

--- src/gdr/xb_format.hpp
+++ src/gdr/xb_format.hpp
@@ -8,6 +8,8 @@
 
 namespace xb_format {
 
+// Cheap magic-byte sniff, used to decide whether a file should be routed to
+// importXB at all.
 bool isXBData(std::span<uint8_t const> data);
 
 std::vector<uint8_t> exportXB(BotReplay const& replay);
